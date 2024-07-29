726e4ec8f4a8aad3a8d1b4d7a6a9504fd5c7aa33
NITianHarsh <harshitverma25304@gmail.com>
2024-07-29 19:32:16 +0530

    disJointset class

--- Graph/disjoinSets.cpp
+++ Graph/disjoinSets.cpp
@@ -26,3 +26,40 @@ void doUnion(vector<int>& arr, int a, int b){ // this join two node a, b
     int b_parent = find(arr, b);
     arr[a_parent] = arr[b_parent];
 }
+
+// 2nd way is a better and a nice way  its complete optimised solution
+
+class disjointSetUnion {
+    // what will be in it
+    vector<int> rank;
+    vector<int> parent;
+public:  // constructor defining
+    disjointSetUnion(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        for(int i=0; i<n+1; i++){
+            parent[i] = i;
+        }
+    }
+// what will it do 
+    int findPar(int n) {
+        if(n == parent[n]) return n;
+        return parent[n] = findPar(parent[n]);
+    }
+
+    void unionOperation(int x, int y) {
+        int xP = findPar(x);
+        int yP = findPar(y);
+
+        if(xP == yP) return ; // means belongs to same component
+        
+        if(rank[xP] < rank[yP]) {
+            parent[x] = y;
+        } else if(rank[xP] < rank[yP]) {
+            parent[y] = x;
+        } else {
+            parent[y] = x;
+            rank[yP]++;
+        }
+    }
+};
